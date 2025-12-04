02c88ba719be18a5d0496ae4ec62b794816a71bf
Riiskager <emil.axel.riis@gmail.com>
2025-12-04 12:00:01 +0100

    Burgermenu

--- src/components/Burgermenu.jsx
+++ src/components/Burgermenu.jsx
@@ -1,12 +1,35 @@
+import { useState } from "react";
 import '../css/burgermenu.css'
 
-export default function Burgermenu(){
-    return(
-        <div className="burgermenu">
-        <div className="burger burger 1"></div>
-        <div className="burger burger 2"></div>
-        <div className="burger burger 3"></div>
-        </div>
-    )
 
+export default function BurgerMenu() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <button 
+        className="burger"
+        onClick={() => setOpen(!open)}
+      >
+        {open ? "✕" : "☰"}
+      </button>
+
+      {/* desktop nav */}
+      <nav className="desktop-nav">
+        {/*Navlinks */}
+      </nav>
+
+      {open && (
+        <div className="mobile-menu">
+          {/* menu items */}
+          <ul>
+            <li>Program</li>
+            <li>Program</li>
+            <li>Program</li>
+            <li>Program</li>
+          </ul>
+        </div>
+      )}
+    </>
+  );
 }
