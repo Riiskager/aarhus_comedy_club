4cd487636b369c888c3622b7946b3d0c065b2df8
Riiskager <emil.axel.riis@gmail.com>
2025-12-04 13:16:46 +0100

    Header træls, men virker

--- src/components/Burgermenu.jsx
+++ src/components/Burgermenu.jsx
@@ -4,29 +4,50 @@ import '../css/burgermenu.css'
 
 export default function BurgerMenu() {
   const [open, setOpen] = useState(false);
+  const [closing, setClosing] = useState(false);
+  const toggleMenu = () => {
+    if (open) {
+      // start closing animation
+      setClosing(true);
+      // remove menu after animation duration (500ms)
+      setTimeout(() => {
+        setOpen(false);
+        setClosing(false);
+      }, 450);
+    } else {
+      setOpen(true); // open menu
+    }
+  };
 
   return (
     <>
       <button 
         className="burger"
-        onClick={() => setOpen(!open)}
+        onClick={toggleMenu}
       >
         {open ? "✕" : "☰"}
       </button>
+      
+        
+
 
       {/* desktop nav */}
       <nav className="desktop-nav">
         {/*Navlinks */}
       </nav>
 
-      {open && (
-        <div className="mobile-menu">
-          {/* menu items */}
+       {(open || closing) && (
+        <div
+          className={`mobile-menu ${open && !closing ? "open" : ""} ${
+            closing ? "closing" : ""
+          }`}
+        >
           <ul>
             <li>Program</li>
-            <li>Program</li>
-            <li>Program</li>
-            <li>Program</li>
+            <li>Komikere</li>
+            <li>Booking</li>
+            <li>Om os</li>
+            <li>Gavekort</li>
           </ul>
         </div>
       )}
