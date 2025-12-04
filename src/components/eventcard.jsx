70c7ac2a072342d58cc23aa12d2e58c2bbee2838
Riiskager <emil.axel.riis@gmail.com>
2025-12-04 10:05:53 +0100

    Update eventcard

--- src/components/eventcard.jsx
+++ src/components/eventcard.jsx
@@ -4,6 +4,9 @@ export default function Eventcard( { event } ){
         <article className="show">
             <img src={event.img} alt={event.titel}></img>
             <h1>{event.titel}</h1>
+            <p>{event.kort_beskrivelse}</p>
+            <p>{event.dato.toDate().toLocaleDateString()}</p>
+
             <p>hej</p>
         </article>
     )
