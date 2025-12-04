178ecb1ca3beda21dc2a6dda6deb9a8f3c8eb5c3
MieZilmer <158352685+MieZilmer@users.noreply.github.com>
2025-12-04 13:17:49 +0100

    slettet vite og react

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -49,7 +49,11 @@ export default function Footer() {
         </a>
       </div>
       <a href="/" aria-label="Forside">
-        <img src="/img/Logo.svg" alt="Aarhus Comedy Club logo" />
+        <img
+          src="/img/Logo.svg"
+          alt="Aarhus Comedy Club logo"
+          className="logo-footer"
+        />
       </a>
       <a href="https://www.findsmiley.dk/1463725" className="fugaz">
         Fødevarestyrelsens kontrolrapport
--- src/components/eventcard.jsx
+++ src/components/eventcard.jsx
@@ -6,7 +6,6 @@ export default function Eventcard( { event } ){
             <h1>{event.titel}</h1>
             <p>{event.kort_beskrivelse}</p>
             <p>{event.dato.toDate().toLocaleDateString()}</p>
-
             <p>hej</p>
         </article>
     )
