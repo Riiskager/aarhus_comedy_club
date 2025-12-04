3b49a74c97458e6f0f707f9e43661cf35009ec93
MieZilmer <158352685+MieZilmer@users.noreply.github.com>
2025-12-04 10:27:34 +0100

    footer

--- src/pages/forside.jsx
+++ src/pages/forside.jsx
@@ -2,33 +2,34 @@ import { useEffect, useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../firebase";
 import Eventcard from "../components/eventcard";
+import Footer from "../components/footer";
 
-export default function Forside(){
+export default function Forside() {
+  const [events, setEvents] = useState([]);
 
-    const [events, setEvents] = useState([]);
+  useEffect(() => {
+    async function LoadEvents() {
+      const ref = collection(db, "events");
+      const snap = await getDocs(ref);
 
-    useEffect(() => {
-        async function LoadEvents() {
-            const ref = collection(db, "events");
-            const snap = await getDocs(ref);
+      const list = snap.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
 
-            const list = snap.docs.map(doc => ({
-                id: doc.id,
-                ...doc.data()
-            }));
+      console.log(list);
+      setEvents(list);
+    }
 
-            console.log(list);
-            setEvents(list);
-        }
+    LoadEvents();
+  }, []);
 
-        LoadEvents();
-    }, []);
-
-    return (
-        <div>
-            {events.map(event => (
-                <Eventcard key={event.id} event={event} />
-            ))}
-        </div>
-    );
+  return (
+    <div>
+      {events.map((event) => (
+        <Eventcard key={event.id} event={event} />
+      ))}
+      <Footer />
+    </div>
+  );
 }
