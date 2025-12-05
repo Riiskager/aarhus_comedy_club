d56e10b5b0e1a4f81f32a35a19d924281a4a212e
Riiskager <emil.axel.riis@gmail.com>
2025-12-05 14:55:56 +0100

    Burgermenu ændret
    Alt virker nice, god animation
    
    Mangler scrollbehavior
    Links ligger ovenpå kategorier
    mangler links til alle

--- src/components/Burgermenu.jsx
+++ src/components/Burgermenu.jsx
@@ -4,7 +4,6 @@ import Burger from "./Burger";
 
 export default function BurgerMenu() {
     const [open, setOpen] =useState(false);
-   
     useEffect(() =>{
         if(open){
             document.body.style.overflow="hidden";
@@ -14,6 +13,8 @@ export default function BurgerMenu() {
 
     },[open])
 
+    const [openSubmenu, setOpenSubmenu] = useState(null);   
+
   return (
     <>
    <Burger open={open} setOpen={setOpen} />
@@ -21,9 +22,32 @@ export default function BurgerMenu() {
    <div className={`menu ${open ? "open" : ""}`}>
     <ul>
         <li className="menuitem"> <a href="/"> Program</a></li>
-        <li className="menuitem">Komikere</li>
-        <li className="menuitem">Booking</li>
-        <li className="menuitem">Om os</li>
+        <li onClick={() => setOpenSubmenu(openSubmenu === "komikere" ? null : "komikere")}
+            className={`submenu ${openSubmenu === "komikere" ? "open" : ""}`}>
+            Komikere
+        <ul className={`submenuitem ${openSubmenu === "komikere" ? "open" : ""}`}>
+            <li className="komikerliste"><a href="/komikerliste">Komikerliste</a></li>
+            <li className="impro-holdet"><a href="/impro-holdet">Impro-holdet</a></li>
+        </ul>
+        </li>
+
+        <li onClick={() => setOpenSubmenu(openSubmenu === "booking" ? null : "booking")}
+            className={`submenu ${openSubmenu === "booking" ? "open" : ""}`}>
+            Booking
+        <ul className={`submenuitem ${openSubmenu === "booking" ? "open" : ""}`}>
+            <li className="komikerliste">Komikerliste</li>
+            <li className="impro-holdet">Impro-holdet</li>
+        </ul>
+         </li>
+        <li onClick={() => setOpenSubmenu(openSubmenu === "omos" ? null : "omos")}
+            className={`submenu ${openSubmenu === "omos" ? "open" : ""}`}>
+            Om os
+        <ul className={`submenuitem ${openSubmenu === "omos" ? "open" : ""}`}>
+            <li className="komikerliste">Komikerliste</li>
+            <li className="impro-holdet">Impro-holdet</li>
+        </ul>
+      
+        </li>
         <li className="menuitem">Gavekort</li>
      
     </ul>   
