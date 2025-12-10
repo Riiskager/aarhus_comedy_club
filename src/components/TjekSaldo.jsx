d7f2ee792f18012cef09b84cc60af0f55351ee2d
MieZilmer <158352685+MieZilmer@users.noreply.github.com>
2025-12-10 12:59:55 +0100

    Banner, gavekort

--- src/components/TjekSaldo.jsx
+++ src/components/TjekSaldo.jsx
@@ -5,7 +5,7 @@ export default function TjekSaldo(){
     return(
        <div className="formular">
             <form>
-                <h1>Tjek Saldo</h1>
+                <h1 id="saldo" >Tjek Saldo</h1>
                 <input className="formaal" placeholder="indtast kode" type="number">
                 </input>
                 <button className='knap'>Tjek Saldo</button>
