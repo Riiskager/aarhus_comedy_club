41e6e6dd00d4b7948e3415cbda8c00e99324b69a
Riiskager <emil.axel.riis@gmail.com>
2025-12-11 09:52:29 +0100

    prøvede noget, it didnt work

--- src/components/KontaktFormular.jsx
+++ src/components/KontaktFormular.jsx
@@ -1,22 +1,24 @@
 import '../css/kontakt.css'
 
 export default function KontaktFormular(){
+
     return(
         <div className='formular'>
         <form>
             <h1>Kontaktformular</h1>
             <label>
-                <select className='formaal'>
+                <select className='formaal' navn="formaal">
                     <option value="1">Vælg et formål</option>
                     <option value="2">Julefrokost</option>
                     <option value="3">Lej lokalet</option>
                     <option value="4">Book os</option>
                     <option value="5">Almindelig spørgsmål</option>
+                    <option value="6">Impro-holdet</option>
                 </select>
             </label>
-            <input className="navnmail" placeholder='Indtast navn...'/>
-            <input className="navnmail" placeholder='Indtast e-mail...'/>
-            <input className="besked" placeholder='Skriv besked...'/>
+            <input name="navn" className="navnmail" placeholder='Indtast navn...'/>
+            <input name="email" className="navnmail" placeholder='Indtast e-mail...'/>
+            <input name="besked" className="besked" placeholder='Skriv besked...'/>
             <button className="knap" type='submit'>Send besked</button>
         </form>
         </div>
