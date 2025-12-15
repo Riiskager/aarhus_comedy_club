dc47fad3aeb8f87ac884d0ab41a668ce16dbd26d
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2025-12-15 15:40:17 +0100

    RACE: Refactor routing and import statements in App and main components

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,44 @@
-
-import './css/App.css'
-import Forside from './pages/forside'
-import Header from './components/header'
-import { BrowserRouter, Routes, Route } from 'react-router'
-import Kontakt from './pages/Kontakt'
-import Gavekort from './pages/Gavekort'
-import Footer from './components/footer'
-import BookingInfo from './components/bookinginfo'
-import Komikerliste from './pages/komikerliste'
-import Event from './pages/Event'
-import Opret from './pages/Opret'
-import Program from './pages/program'
-import Improholdet from './pages/Improholdet'
-import Omos from './pages/omos'
-import Betal from './pages/Betal'
-import Bekræft from './pages/bekaeft'
-import Faq from './pages/faq'
+import { Route, Routes } from "react-router";
+import BookingInfo from "./components/bookinginfo";
+import Footer from "./components/footer";
+import Header from "./components/header";
+import "./css/App.css";
+import Betal from "./pages/Betal";
+import Event from "./pages/Event";
+import Gavekort from "./pages/Gavekort";
+import Improholdet from "./pages/Improholdet";
+import Kontakt from "./pages/Kontakt";
+import Opret from "./pages/Opret";
+import Bekræft from "./pages/bekaeft";
+import Faq from "./pages/faq";
+import Forside from "./pages/forside";
+import Komikerliste from "./pages/komikerliste";
+import Omos from "./pages/omos";
+import Program from "./pages/program";
 
 function App() {
- 
- return (
-    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/aarhus_comedy_club"}>
+  return (
+    <>
+      {" "}
       <Header />
       <Routes>
         <Route path="/kontakt" element={<Kontakt />} />
         <Route path="*" element={<Forside />} />
-        <Route path="/gavekort" element={<Gavekort/>}/>
-        <Route path="/booking" element={<BookingInfo/>}/>
-        <Route path="/komikerliste" element={<Komikerliste/>}/>
-        <Route path="/event/:id" element={<Event/>}/>
-        <Route path="/admin" element={<Opret/>}/>
-        <Route path="/program" element={<Program/>}/>
-        <Route path="/impro-holdet" element={<Improholdet/>}/>
-        <Route path="/historie" element={<Omos/>}/>
-        <Route path="/betaling" element={<Betal/>}/>
-        <Route path="/bekræft" element={<Bekræft/>}/>
-        <Route path="/faq" element={<Faq/>}/>
-
+        <Route path="/gavekort" element={<Gavekort />} />
+        <Route path="/booking" element={<BookingInfo />} />
+        <Route path="/komikerliste" element={<Komikerliste />} />
+        <Route path="/event/:id" element={<Event />} />
+        <Route path="/admin" element={<Opret />} />
+        <Route path="/program" element={<Program />} />
+        <Route path="/impro-holdet" element={<Improholdet />} />
+        <Route path="/historie" element={<Omos />} />
+        <Route path="/betaling" element={<Betal />} />
+        <Route path="/bekræft" element={<Bekræft />} />
+        <Route path="/faq" element={<Faq />} />
       </Routes>
-      <Footer/>
-    </BrowserRouter>
+      <Footer />
+    </>
   );
 }
 
-export default App
+export default App;
