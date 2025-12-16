92f002584cc0f9d96b68b82c84c98dee7ba67d96
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2025-12-16 07:32:08 +0100

    RACE: Update base path in Vite config for production deployment

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig(({ command }) => {
   return {
     plugins: [react()],
-    base: command === "serve" ? "/" : "/post-app-with-firebase/"
+    base: command === "serve" ? "/" : "/aarhus_comedy_club/"
   };
 });
