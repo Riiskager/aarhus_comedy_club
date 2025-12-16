adfad587c764a33970af77b65b6654d697073963
Riiskager <emil.axel.riis@gmail.com>
2025-12-16 13:00:54 +0100

    sletter lukkefunktionen, det fuckede med andet

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import Burgermenu from "./Burgermenu";
+import BurgerMenu from "./burgermenu";
 import "../css/header.css";
 import { Link } from "react-router";
 
@@ -10,7 +10,7 @@ export default function Header() {
         <img src="img/home.svg" alt="home" className="hjemknap"></img>
       </Link>
       <p>Aarhus Comedy Club</p>
-      <Burgermenu />
+      <BurgerMenu />
     </div>
   );
 }
